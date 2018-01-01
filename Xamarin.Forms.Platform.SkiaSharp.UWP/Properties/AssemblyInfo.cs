29d3f16e547606d9c26dbf1d047656c3c88a40bb
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-01-01 14:20:26 +0100

    Fixed UWP project name and path

--- Xamarin.Forms.Platform.SkiaSharp.UWP/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Platform.SkiaSharp.UWP/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Xamarin.Forms.Platform.UWP")]
+[assembly: AssemblyTitle("Xamarin.Forms.Platform.SkiaSharp.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xamarin.Forms.Platform.UWP")]
+[assembly: AssemblyProduct("Xamarin.Forms.Platform.SkiaSharp.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
